5984ea8fff7f490a597afd9d129c35eb01cfa6f5
yahya <yahyaveziroglu@gmail.com>
2025-12-06 11:44:31 -0500

    updated

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -4,5 +4,6 @@ public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
         System.out.println("Yes");
+        System.out.println("No");
     }
 }
