98c50c203c7a6a7a44caac490f4c3be1f91a06da
yahya <yahyaveziroglu@gmail.com>
2025-12-06 11:42:24 -0500

    new

---
+++ src/day01/Main.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Change");
+        System.out.println("Hello Yahya");
+        System.out.println("Hello Cydeo");
+    }
+}
