71cc2acc6b5dff34f5b75dea57e45886cf1a854b
Yahya Veziroglu <yahyaveziroglu@gmail.com>
2025-12-09 22:05:27 -0500

    Update AddOne.java
    vzrbg

--- src/day01/AddOne.java
+++ src/day01/AddOne.java
@@ -3,5 +3,7 @@ package day01;
 public class AddOne {
     public static void main(String[] args) {
         System.out.println(1+1);
+        System.out.println(2+1);
+        System.out.println(3+1);
     }
 }
